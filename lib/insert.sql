253c2524067487ecf5f1ffdae1f32a08158ee64a
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-14 06:20:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,24 +1,11 @@
-INSERT INTO users (name, age) VALUES
-('Finnebar', 17),
-('Bear', 6),
-('Iguana', 4),
-('Alex', 33),
-('Amanda', 24),
-('Sophie', 24),
-('Rosey', 9),
-('Victoria', 23),
-('Franz', 100),
-('Hermione', 30),
-('Voldemort', 90),
-('Marisa', 24),
-('Swizzle', 4),
-('Sirius', 36),
-('Albus', 113),
-('Squid', 5),
-('Whale', 6),
-('Pacha', 5),
-('Ena', 24),
-('Katie', 24);
+INSERT INTO users (id, name, age) VALUES 
+(1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4),
+(4, 'Alex', 33), (5, 'Amanda', 24), (6, 'Sophie', 24), 
+(7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), 
+(10, 'Hermione', 30), (11, 'Voldemort', 90), (12, 'Marisa', 24),
+(13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), 
+(16, 'Squid', 5), (17, 'Whale', 6), (18, 'Pacha', 5), 
+(19, 'Ena', 24), (20, 'Katie', 24);
 
 INSERT INTO projects (title,category,funding_goal,start_date,end_date) VALUES
 ("Buy me things","charity",200,"2019-06-12","2019-06-30"),
